65c6a9b71976941a5aae6802b2461ff02ff60edb
Emanuel <alvarezema9@gmail.com>
2022-09-27 10:14:00 -0600

    Se creo el contendor MDI con los menus Abrir, catálogos, procesos, informes, herramientas y ayuda

---
+++ Programa/ModernizacionEmpresa/CapaControlador/Controlador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaModelo;
+using System.Data;
+using System.Data.Odbc;
+
+namespace CapaControlador
+{
+    public class Controlador
+    {
+        Consultas cons = new Consultas();
+
+        // LLenar una tabla CAPA CONTROLADOR
+        public DataTable llenarTbl(string tabla)
+        {
+            OdbcDataAdapter dt = cons.llenarTbl(tabla);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+
+    }
+}
